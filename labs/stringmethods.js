052bfa9c305a950cfe64c48a1e6a5908922f1ee9
Zakaria Merfouk <59699467+zachyboy-92@users.noreply.github.com>
2021-08-04 18:04:43 -0400

    Add files via upload

---
+++ labs/stringmethods.js
@@ -0,0 +1,53 @@
+const prompt = require("prompt-sync")();
+
+// Question #1
+// let inputString = prompt("Insert string: ");
+// let inputValue = prompt("Select value");
+// let occurences = 0;
+// let valueIndex = [];
+// for (let i = 0; i < inputString.length; i++) {
+//   if (inputString[i].includes(inputValue)) {
+//     occurences++;
+//     valueIndex.push(i + 1);
+//   }
+// }
+// console.log(
+//   `The value ${inputValue} has occurred ${
+//     occurences > 1 ? `${occurences} times` : `${occurences} time`
+//   } in index ${valueIndex}`
+// );
+
+// if (!foundValue) {
+//   console.log("Value not found");
+// }
+
+// Question #2
+// let userInput = prompt("Insert value: ");
+// let vowels = ["a", "e", "i", "o", "u"];
+// let numberOfVowels = 0;
+// userInput.replace(" ", "");
+// console.log(userInput);
+// for (let i = 0; i < userInput.length; i++) {
+//   for (var j = 0; j < vowels.length; j++) {
+//     if (userInput[i].includes(vowels[j])) {
+//       numberOfVowels++;
+//     }
+//   }
+// }
+// let numberOfConsonants = userInput.length - 1 - numberOfVowels;
+
+// console.log(`Number Of vowels: ${numberOfVowels}`);
+// console.log(`Number Of consonants: ${numberOfConsonants}`);
+
+//Question 3
+let userInput = prompt("Insert Value: ");
+let palindrome = "";
+for (let i = userInput.length - 1; i >= 0; i--) {
+  palindrome += userInput.charAt(i);
+}
+console.log(palindrome);
+if (userInput === palindrome) {
+  console.log(userInput + " is a palindrome string");
+} else {
+  console.log(userInput + " is not a palindrome string");
+}
