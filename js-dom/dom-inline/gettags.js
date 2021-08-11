eeea483a44d378f269f81fe760fe358acc740176
Zakaria Merfouk <59699467+zachyboy-92@users.noreply.github.com>
2021-08-11 17:54:24 -0400

    Add files via upload

---
+++ js-dom/dom-inline/gettags.js
@@ -0,0 +1,6 @@
+// document
+//   .querySelector("div")
+//   .insertAdjacentHTML("afterend", "<p>My name is Zakaria</p>");
+const header = document.getElementById("parent");
+// header.innerHTML = "My name is Zak";
+// header.textContent = "Hello world";
